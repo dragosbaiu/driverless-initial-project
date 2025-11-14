2cf040276377751e052bf715c2a4dc6c9595eba9
dragos <dragosbaiuandrei@gmail.com>
2025-11-15 00:59:05 +0100

    docs: add simulation images

--- src/main.cpp
+++ src/main.cpp
@@ -26,25 +26,25 @@ int main() {
     double pathRoundedMax;
     double roundedMax;
 
-    // Simulate and visualize standard bicycle model
+    //Simulate and visualize standard bicycle model
     Vehicle base = vehicle;
     roundedMax = simulateBase(base, environment);
-    runApp(base, roundedMax, "Trajectory of Standard Bicycle Model");
+    runApp(base, roundedMax, "Trajectory of Standard Vehicle using Kinematic Bycicle Model");
 
     // Simulate and visualize bicycle model with lateral drift
     Vehicle baseDrift = vehicle;
     roundedMax = simulateBaseDrift(baseDrift, environment);
-    runApp(baseDrift, roundedMax, "Trajectory of Standard Bicycle Model with Lateral Drift");
+    runApp(baseDrift, roundedMax, "Trajectory of Standard Vehicle with Lateral Drift");
 
     // Simulate and visualize bicycle model controlled to have a straight heading
     Vehicle headingStraight = vehicle;
     roundedMax = simulateHeadingStraight(headingStraight, environment, controller);
-    runApp(headingStraight, roundedMax, "Trajectory of Bicycle Controlled to Have a Straight Heading");
+    runApp(headingStraight, roundedMax, "Trajectory of Vehicle Controlled to Have a Straight Heading");
 
     // Simulate and visualize bicycle model controlled to have a straight heading, with noise and lateral drift
     Vehicle headingStraightDrift = vehicle;
     roundedMax = simulateHeadingStraightDrift(headingStraightDrift, environment, controller);
-    runApp(headingStraightDrift, roundedMax, "Trajectory of Bicycle Controlled to Have a Straight Heading, with Noise and Lateral Drift");
+    runApp(headingStraightDrift, roundedMax, "Trajectory of Vehicle Controlled to Have a Straight Heading, with Noise and Lateral Drift");
 
 
     // Generate and visualize a straight path   
@@ -60,7 +60,7 @@ int main() {
     pStraight.setInitialX(straightPath.x.front());
     pStraight.setInitialY(straightPath.y.front());
     roundedMax = simulateP(pStraight, straightPath, environment, controller);
-    runApp(pStraight, straightPath, max (pathRoundedMax, roundedMax), "Straight Trajectory of Bicycle: P Controller");
+    runApp(pStraight, straightPath, max (pathRoundedMax, roundedMax), "Straight Trajectory of Vehicle: P Controller");
 
     // PID controller for following the path
     Vehicle pidStraight = vehicle;
@@ -69,7 +69,7 @@ int main() {
     pidStraight.setInitialX(straightPath.x.front());
     pidStraight.setInitialY(straightPath.y.front());
     roundedMax = simulatePID(pidStraight, straightPath, environment, controller);
-    runApp(pidStraight, straightPath, max (pathRoundedMax, roundedMax), "Straight Trajectory of Bicycle: PID Controller");
+    runApp(pidStraight, straightPath, max (pathRoundedMax, roundedMax), "Straight Trajectory of Vehicle: PID Controller");
 
     // Stanley controller for following the path
     Vehicle stanleyStraight = vehicle;
@@ -77,7 +77,7 @@ int main() {
     stanleyStraight.setInitialX(straightPath.x.front());
     stanleyStraight.setInitialY(straightPath.y.front());
     roundedMax = simulateStanley(stanleyStraight, straightPath, environment, controller);
-    runApp(stanleyStraight, straightPath, max (pathRoundedMax, roundedMax), "Straight Trajectory of Bicycle: Stanley Controller");
+    runApp(stanleyStraight, straightPath, max (pathRoundedMax, roundedMax), "Straight Trajectory of Vehicle: Stanley Controller");
 
 
     // Generate and visualize a circular path
@@ -94,7 +94,7 @@ int main() {
     pCircular.setInitialX(circularPath.x.front());
     pCircular.setInitialY(circularPath.y.front());
     roundedMax = simulateP(pCircular, circularPath, environment, controller);
-    runApp(pCircular, circularPath, max(roundedMax, pathRoundedMax), "Circular Trajectory of Bicycle: P Controller");
+    runApp(pCircular, circularPath, max(roundedMax, pathRoundedMax), "Circular Trajectory of Vehicle: P Controller");
 
     // PID controller for following the path
     Vehicle pidCircular = vehicle;
@@ -103,7 +103,7 @@ int main() {
     pidCircular.setInitialX(circularPath.x.front());
     pidCircular.setInitialY(circularPath.y.front());
     roundedMax = simulatePID(pidCircular, circularPath, environment, controller);
-    runApp(pidCircular, circularPath, max(roundedMax, pathRoundedMax), "Circular Trajectory of Bicycle: PID Controller");
+    runApp(pidCircular, circularPath, max(roundedMax, pathRoundedMax), "Circular Trajectory of Vehicle: PID Controller");
 
     // Stanley controller for following the path
     Vehicle stanleyCircular = vehicle;
@@ -111,7 +111,7 @@ int main() {
     stanleyCircular.setInitialX(circularPath.x.front());
     stanleyCircular.setInitialY(circularPath.y.front());
     roundedMax = simulateStanley(stanleyCircular, circularPath, environment, controller);
-    runApp(stanleyCircular, circularPath, max(roundedMax, pathRoundedMax), "Circular Trajectory of Bicycle: Stanley Controller");
+    runApp(stanleyCircular, circularPath, max(roundedMax, pathRoundedMax), "Circular Trajectory of Vehicle: Stanley Controller");
     
 
     // Generate and visualize a Sine path
@@ -127,7 +127,7 @@ int main() {
     pSine.setInitialX(sinePath.x.front());
     pSine.setInitialY(sinePath.y.front());
     roundedMax = simulateP(pSine, sinePath, environment, controller);
-    runApp(pSine ,sinePath, max(roundedMax, pathRoundedMax), "Sine Trajectory of Bicycle: P Controller");
+    runApp(pSine ,sinePath, max(roundedMax, pathRoundedMax), "Sine Trajectory of Vehicle: P Controller");
 
     // PID controller for following the path
     Vehicle pidSine = vehicle;
@@ -136,7 +136,7 @@ int main() {
     pidSine.setInitialX(sinePath.x.front());
     pidSine.setInitialY(sinePath.y.front());
     roundedMax = simulatePID(pidSine, sinePath, environment, controller);
-    runApp(pidSine ,sinePath, max(roundedMax, pathRoundedMax), "Sine Trajectory of Bicycle: PID Controller");
+    runApp(pidSine ,sinePath, max(roundedMax, pathRoundedMax), "Sine Trajectory of Vehicle: PID Controller");
 
     // Stanley controller for following the path
     Vehicle stanleySine = vehicle;
@@ -144,7 +144,7 @@ int main() {
     stanleySine.setInitialX(sinePath.x.front());
     stanleySine.setInitialY(sinePath.y.front());
     roundedMax = simulateStanley(stanleySine, sinePath, environment, controller);
-    runApp(stanleySine ,sinePath, max(roundedMax, pathRoundedMax), "Sine Trajectory of Bicycle: Stanley Controller");
+    runApp(stanleySine ,sinePath, max(roundedMax, pathRoundedMax), "Sine Trajectory of Vehicle: Stanley Controller");
 
 
     // Generate and visualize a chicane path
@@ -161,7 +161,7 @@ int main() {
     pidChicane.setInitialX(chicanePath.x.front());
     pidChicane.setInitialY(chicanePath.y.front());   
     roundedMax = simulatePID(pidChicane, chicanePath, environment, controller);
-    runApp(pidChicane ,chicanePath, max(roundedMax, pathRoundedMax), "Chicane Trajectory of Bicycle: PID Controller");
+    runApp(pidChicane ,chicanePath, max(roundedMax, pathRoundedMax), "Chicane Trajectory of Vehicle: PID Controller");
 
     // Uses Stanley controller for following the path
     Vehicle stanleyChicane = vehicle;
@@ -169,7 +169,7 @@ int main() {
     stanleyChicane.setInitialX(chicanePath.x.front());
     stanleyChicane.setInitialY(chicanePath.y.front());   
     roundedMax = simulateStanley(stanleyChicane, chicanePath, environment, controller);
-    runApp(stanleyChicane ,chicanePath, max(roundedMax, pathRoundedMax), "Chicane Trajectory of Bicycle: Stanley Controller");
+    runApp(stanleyChicane ,chicanePath, max(roundedMax, pathRoundedMax), "Chicane Trajectory of Vehicle: Stanley Controller");
 
     return 0;
 }
